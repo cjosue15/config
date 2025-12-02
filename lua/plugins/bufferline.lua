f62dfebe0300fb267fcbf4c3861fbaec0414ac3c
Josue Morales <dev.carlos.morales@gmail.com>
2025-12-02 08:52:11 -0500

    update: add new way how to move position of buffer

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>b[", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "<leader>b]", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+  },
+}
